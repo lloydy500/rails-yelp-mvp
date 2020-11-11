d2b762cfa27fcee4f7ef72ce9927b5307aeba75c
Lloyd Chapple <lloydchaps@gmail.com>
2020-11-11 16:36:48 +0000

    restaurants with reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"],
     message: "%{value is not a valid language}" }
+  validates_presence_of :name, on: :create, message: "can't be blank"
+  validates_presence_of :address, on: :create, message: "can't be blank"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_151955) do
+ActiveRecord::Schema.define(version: 2020_11_11_153406) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
